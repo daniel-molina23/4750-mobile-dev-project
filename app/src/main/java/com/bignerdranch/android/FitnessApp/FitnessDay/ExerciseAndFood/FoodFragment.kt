3db4730915c35ea14bff0b1dc1a5c4e31e207e9f
Daniel Molina <dannyfacio14@gmail.com>
2020-11-29 14:37:14 -0800

    Changes to db converters, BUGFIX: food & exercise

--- app/src/main/java/com/bignerdranch/android/FitnessApp/FitnessDay/ExerciseAndFood/FoodFragment.kt
+++ app/src/main/java/com/bignerdranch/android/FitnessApp/FitnessDay/ExerciseAndFood/FoodFragment.kt
@@ -92,8 +92,13 @@ class FoodFragment(fitnessDay: FitnessDay) : Fragment()
 
         //Switching Back to the FitnessDayFragment
         this.menuButton.setOnClickListener { view ->
+//            if(ensureProperCalorieInput())
+//                this.passData(FragmentToSwitchTo.FITNESS_DAY_FRAGMENT, fitnessDay)
+            /**
+             * Pop from backstack (delete this fragment and go to previous, aka FitnessDayFragment)
+             */
             if(ensureProperCalorieInput())
-                this.passData(FragmentToSwitchTo.FITNESS_DAY_FRAGMENT, fitnessDay)
+                fragmentManager!!.popBackStack()
         }
 
 
